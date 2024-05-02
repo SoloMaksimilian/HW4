f4d96c76d573c157e4e6b05b6c41e21b51e2ae15
SoloMaksimilian <mephistosmad@gmail.com>
2024-05-02 20:21:27 +0300

    Ex1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int age = 18;
+        boolean maturity = age >=18;
+         if (maturity) {
+             System.out.println("Если возраст человека равен " + age + ",то он совершеннолетний");
+         }
+         else {System.out.println("Если возраст человека равен " + age + ",то он не достиг совершеннолетия, нужно немного подождать");
+         }
+
+
     }
 }
