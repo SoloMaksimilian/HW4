f25b30ff7aabc5a83b840b6ad4c6104b205525cd
SoloMaksimilian <mephistosmad@gmail.com>
2024-05-06 22:08:48 +0300

    Ex7

--- src/Main.java
+++ src/Main.java
@@ -12,10 +12,7 @@ public class Main {
         if (temperature > 5) {
             System.out.println("На улице " + temperature + " градусов, можно идти без шапки");
         }
-        if (temperature <5) {
-            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку");
-        }
-        if (temperature == 5) {
+               if (temperature == 5) {
             System.out.println("На улице " + temperature + " градусов, шапку нужна взять с собой");
         }
 
@@ -34,8 +31,7 @@ public class Main {
         if (year >= 2 && year <= 6 ) {
             System.out.println("Если возраст человека равен " + year + ", то ему нужно ходить в садик");
         }
-        if (year >= 7 && year <= 17 ) {
-            System.out.println("Если возраст человека равен " + year + ", то ему нужно ходить в школу");
+        if (year >= 7 && year <= 17 ) {            System.out.println("Если возраст человека равен " + year + ", то ему нужно ходить в школу");
         }
         if (year >= 18 && year <= 24 ) {
             System.out.println("Если возраст человека равен " + year + ", то ему нужно ходить в университет");
@@ -55,31 +51,43 @@ public class Main {
             System.out.println("Если возраст ребенка равен " + ageChild + ", то ему можно кататься на аттракционе без сопровождения");
         }
 
-        int numberOfPeople = 103;
-        if(numberOfPeople <=60) {
-            System.out.println("Ваше место в очереди №" + numberOfPeople + ", оно сидячее");
+        int numberOfPeople = 102;
+        if(numberOfPeople <60) {
+            int seat = 60 - numberOfPeople;
+            int standing = 102 - 60;
+            System.out.println("В вагоне осталось " + seat + " сидячих мест и " + standing + " стоячих мест");
         }
         else
-            if (numberOfPeople > 60 && numberOfPeople <= 102) {
-            System.out.println("Ваше место в очереди №" + numberOfPeople + ", оно стоячее"); }
+            if (numberOfPeople >= 60 && numberOfPeople < 102) {
+                int standing = 102 - numberOfPeople;
+            System.out.println("В вагоне осталось " + standing + " стоячих мест, седячих не осталось"); }
             else {
-            System.out.println("Ваше место в очереди №" + numberOfPeople + ", вы не попадаете в вагон");
+            System.out.println("В вагоне не осталось мест");
             }
 
-        int one = 1;
-        int two = 2;
-        int three = 3;
+        int one = 4;
+        int two = 4;
+        int three = 4;
         if(one > two && one> three) {
-            System.out.println("Число One, равное " + one + " больше числа two, равного " + two + " и больше числа three, равного " + three);
+            System.out.println("Число 'one', равное " + one + " больше числа 'two', равного " + two + " и больше числа 'three', равного " + three);
         }
         else if (two > one && two> three) {
-            System.out.println("Число two, равное " + two + " больше числа one, равного " + one + " и больше числа three, равного " + three);
+            System.out.println("Число 'two', равное " + two + " больше числа 'one', равного " + one + " и больше числа 'three', равного " + three);
         }
         else if (three>one && three>two) {
-            System.out.println("Число three, равное " + three + " больше числа one, равного " + one + " и больше числа two, равного " + two);
+            System.out.println("Число 'three', равное " + three + " больше числа 'one', равного " + one + " и больше числа 'two', равного " + two);
+        }
+        else if (one == two && one > three){
+            System.out.println("Числа 'one' и 'two' равны " + one + ", и они больше числа 'three', равного " + three);
+        }
+        else if (one == three && one > two){
+            System.out.println("Числа 'one' и 'three' равны " + one + ", и они больше числа 'two', равного " + two);
+        }
+        else if (three == two && two > one){
+            System.out.println("Числа 'three' и 'two' равны " + two + ", и они больше числа 'one', равного " + one);
         }
         else {
-            System.out.println("Есть минимум пара одинаковый числел");
+            System.out.println("Все числа равны");
         }
 
 
